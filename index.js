897cbc0eb4b4bab7a0203596dd400d7fff1095d6
elijahmm17 <elijahmm17@gmail.com>
2019-09-29 17:15:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ class Passenger {
         console.log('this trip' + trip);
         console.log('trip driver wo f call' + trip.driver);
         console.log('called trip driver f' + trip.driver().name);
-        trip => trip.driver()
+        trip => trip.driver();
       }.bind(this)
     );
   }
